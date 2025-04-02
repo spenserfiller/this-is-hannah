fb56588778010887d083fbcfb1ae94236f862061
Spenser Filler <spenserfiller@MacBook-Pro.lan>
2025-04-01 21:04:23 -0500

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+module.exports = function(eleventyConfig) {
+    // Copy asset directories to output
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("img");
+    
+    // Add date filters
+    eleventyConfig.addFilter("dateIso", date => {
+      return date.toISOString().split('T')[0];
+    });
+    
+    eleventyConfig.addFilter("dateReadable", date => {
+      return date.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      });
+    });
+    
+    // Create a posts collection
+    eleventyConfig.addCollection("posts", function(collectionApi) {
+      return collectionApi.getFilteredByGlob("posts/*.md");
+    });
+    
+    return {
+      dir: {
+        input: ".",
+        output: "_site",
+        includes: "_includes"
+      }
+    };
+  };
---
+++ posts/first-post.md
@@ -0,0 +1,19 @@
+---
+title: My First Post
+date: 2025-04-01
+layout: post.njk
+tags: posts
+---
+
+This is my first blog post!
+
+Here's some content that will appear on the blog. You can write in Markdown format, which makes it easy to:
+
+* Create lists
+* **Bold text**
+* *Italic text*
+* [Add links](https://example.com)
+
+## Subheadings work too
+
+And you can write as much content as you want here.
